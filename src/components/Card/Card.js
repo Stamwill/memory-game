28e3dedd39d31fd76a2ab1309cde50ba6533e9f1
William <williamstam@gmail.com>
2022-03-17 16:33:48 +0000

    [Card] Add functionality to make cards flip on click

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,26 @@
 import React from "react";
+import classnames from "clsx";
 import propTypes from "prop-types";
 import classes from "./Card.module.css";
 
 const Card = (props) => {
+  const [flipped, setflipped] = React.useState(false);
+
+  const handleClick = () => {
+    setflipped((prevstate) => !prevstate);
+    console.log("clicked");
+  };
+
   const { children } = props;
-  return <div className={classes.root}>{children}</div>;
+  return (
+    <div className={classes.root} onClick={handleClick}>
+      <div className={classnames(classes.card, { [classes.flipped]: flipped })}>
+        <div className={classes.front}>front</div>
+        <div className={classes.back}>back</div>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 Card.propTypes = {
