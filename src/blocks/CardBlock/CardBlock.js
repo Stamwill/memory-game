05d1c18ffe9f11cf9eed98f8cacce7e5ec50fe79
William <williamstam@gmail.com>
2022-03-17 12:56:15 +0000

    [CardBlock] Add buttons and state to swap between difficulties

--- src/blocks/CardBlock/CardBlock.js
+++ src/blocks/CardBlock/CardBlock.js
@@ -2,14 +2,43 @@ import React from "react";
 import classes from "./CardBlock.module.css";
 import Card from "components/Card";
 
-const arr = [1, 2, 3, 4, 5, 6];
+const easy = [1, 2, 3, 4, 5, 6];
+const intermediate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
 const CardBlock = () => {
+  const [easyDiff, setEasy] = React.useState(null);
+  const [intermediateDiff, setIntermediate] = React.useState(null);
+  const [impossibleDiff, setImpossible] = React.useState(null);
+
+  const handleEasy = () => {
+    setIntermediate(false);
+    setEasy(true);
+  };
+
+  const handleIntermediate = () => {
+    setEasy(false);
+    setIntermediate(true);
+  };
+
+  console.log(easyDiff);
   return (
     <section className={classes.root}>
-      {/* Easy difficulty, 6 images */}
-      {arr.map((item, idx) => (
-        <Card key={idx} />
-      ))}
+      <div className={classes.buttons}>
+        <button className={classes.button} onClick={handleEasy}>
+          Easy
+        </button>
+        <button className={classes.button} onClick={handleIntermediate}>
+          Intermediate{" "}
+        </button>
+      </div>
+      <div className={classes.cards}>
+        {/* Easy difficulty, 6 images */}
+        {easyDiff ? easy.map((idx) => <Card key={idx} />) : null}
+        {/* Intermediate difficulty, 12 images */}
+        {intermediateDiff
+          ? intermediate.map((idx) => <Card key={idx} />)
+          : null}
+      </div>
     </section>
   );
 };
